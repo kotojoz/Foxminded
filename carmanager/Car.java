788686ebae195d79d7642981837ee51aac7ccf4a
Stanislavs Ostrovskis <st.ostrovskis@gmail.com>
2022-10-13 21:59:15 +0300

    Task 2.2 Closes #2

--- carmanager/Car.java
+++ carmanager/Car.java
@@ -1,4 +1,15 @@
 package carmanager;
 
 public class Car {
+
+    public String name;
+
+    public int yearOfProduction;
+
+    public int price;
+
+    public int weight;
+
+    public String color;
+
 }
--- carmanager/Motorcycle.java
+++ carmanager/Motorcycle.java
@@ -1,4 +1,19 @@
 package carmanager;
 
 public class Motorcycle {
+
+    public String name;
+
+    public int yearOfProduction;
+
+    public int price;
+
+    public int weight;
+
+    public String color;
+
+    public String engineType;
+
+    public boolean isReadyToDrive;
+
 }

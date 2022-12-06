0e5378bca9162f5f77fc11699ebabafc64259a34
kotoj <st.ostrovskis@gmail.com>
2022-12-06 15:37:50 +0200

    Enum Motorcycles(fixed)

--- carmanager/CarManager.java
+++ carmanager/CarManager.java
@@ -3,7 +3,6 @@ package carmanager;
 public class CarManager {
 
     public static void main(String[] args) {
-
         Motorcycle suzuki = Motorcycles.SUZUKI.getMotorcycle();
 
         Motorcycle yamaha = Motorcycles.YAMAHA.getMotorcycle();
